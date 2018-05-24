79dccd7ebc468bde4191464702feee1f0c08007a
codesalad <nguyen094@gmail.com>
2018-05-25 00:28:26 +0200

    status, ip functions

--- cloudomate/hoster/vps/proxhost.py
+++ cloudomate/hoster/vps/proxhost.py
@@ -11,6 +11,7 @@ from builtins import super
 
 import ssl
 import requests
+import dateutil.parser
 
 from forex_python.converter import CurrencyRates
 from future import standard_library
@@ -22,6 +23,18 @@ from cloudomate.hoster.vps.solusvm_hoster import SolusvmHoster
 from cloudomate.hoster.vps.vps_hoster import VpsOption
 
 standard_library.install_aliases()
+from abc import abstractmethod
+from collections import namedtuple
+
+VpsConfiguration = namedtuple('VpsConfiguration', ['ip', 'root_password'])
+VpsStatusResource = namedtuple('VpsStatusResource', ['used', 'total'])
+VpsStatusResourceNone = VpsStatusResource(-1, -1)
+VpsStatus = namedtuple('VpsStatus', ['memory',  # Memory VpsStatusResource in GB
+                                     'storage',  # Storage VpsStatusResource in GB
+                                     'bandwidth',  # Bandwidth VpsStatusResource in GB
+                                     'online',  # Boolean
+                                     'expiration',  # Python Datetime object
+                                     'clientarea'])  # Service info retrieved from the ClientArea (for overriding)
 
 
 class ProxHost(SolusvmHoster):
@@ -37,7 +50,7 @@ class ProxHost(SolusvmHoster):
 
     @staticmethod
     def get_clientarea_url():
-        return 'https://panel.linevast.de/clientarea.php'
+        return ''
 
     @staticmethod
     def get_gateway():
@@ -53,6 +66,24 @@ class ProxHost(SolusvmHoster):
             'user': ['firstname', 'lastname', 'email', 'phonenumber', 'password'],
             'address': ['address', 'city', 'state', 'zipcode'],
         }
+
+    def json_user_config(self):
+        data = {
+            'firstname': self._settings.get('user', "firstname"),
+            'lastname': self._settings.get('user', "lastname"),
+            'username': self._settings.get('user', "username"),
+            'email': self._change_email_provider(self._settings.get('user', "email"), '@gmail.com'),
+            'phonenumber': self._settings.get('user', "phonenumber"),
+            'companyname': self._settings.get('user', "companyname"),
+            'address1': self._settings.get('address', "address"),
+            'city': self._settings.get('address', "city"),
+            'state': self._settings.get('address', "state"),
+            'postcode': self._settings.get('address', "zipcode"),
+            'country': self._settings.get('address', 'countrycode'),
+            'password': self._settings.get('user', "password"),
+            'password2': self._settings.get('user', "password")
+        }
+        return data
 
     '''
     Action methods of the Hoster that can be called
@@ -86,153 +117,31 @@ class ProxHost(SolusvmHoster):
         return list(options)
 
     def get_configuration(self):
-        data = {
-            'firstname': self._settings.get('user', "firstname"),
-            'lastname': self._settings.get('user', "lastname"),
-            'username': self._settings.get('user', "username"),
-            'email': self._change_email_provider(self._settings.get('user', "email"), '@gmail.com'),
-            'phonenumber': self._settings.get('user', "phonenumber"),
-            'companyname': self._settings.get('user', "companyname"),
-            'address1': self._settings.get('address', "address"),
-            'city': self._settings.get('address', "city"),
-            'state': self._settings.get('address', "state"),
-            'postcode': self._settings.get('address', "zipcode"),
-            'country': self._settings.get('address', 'countrycode'),
-            'password': self._settings.get('user', "password"),
-            'password2': self._settings.get('user', "password")
-        }
-
-        res = requests.post(self.BASE_URL+'/getconfiguration', json=data, verify=False)
+        res = requests.post(self.BASE_URL+'/getconfiguration', json=self.json_user_config(), verify=False)
         print(res)
-        config = res.content.decode('utf8')
-        return config
+        config = json.loads(res.content)
+        return VpsConfiguration(config['ip'], config['root_password'])
 
     def get_status(self):
-        data = {
-            'firstname': self._settings.get('user', "firstname"),
-            'lastname': self._settings.get('user', "lastname"),
-            'username': self._settings.get('user', "username"),
-            'email': self._change_email_provider(self._settings.get('user', "email"), '@gmail.com'),
-            'phonenumber': self._settings.get('user', "phonenumber"),
-            'companyname': self._settings.get('user', "companyname"),
-            'address1': self._settings.get('address', "address"),
-            'city': self._settings.get('address', "city"),
-            'state': self._settings.get('address', "state"),
-            'postcode': self._settings.get('address', "zipcode"),
-            'country': self._settings.get('address', 'countrycode'),
-            'password': self._settings.get('user', "password"),
-            'password2': self._settings.get('user', "password")
-        }
-
-        res = requests.post(self.BASE_URL+'/getstatus', json=data, verify=False)
+        res = requests.post(self.BASE_URL+'/getstatus', json=self.json_user_config(), verify=False)
         print(res)
-        status = res.content.decode('utf8')
-        return status
+        status = json.loads(res.content.decode('utf8'))
+        return VpsStatus(
+            VpsStatusResourceNone,
+            VpsStatusResourceNone,
+            VpsStatusResourceNone,
+            status['online'],  # online
+            dateutil.parser.parse(status['expiration']),
+            VpsStatusResourceNone
+        )
 
     def purchase(self, wallet, option):
-        data = {
-            'vmid': option.purchase_url,
-            'price': option.price,
-            'firstname': self._settings.get('user', "firstname"),
-            'lastname': self._settings.get('user', "lastname"),
-            'username': self._settings.get('user', "username"),
-            'email': self._change_email_provider(self._settings.get('user', "email"), '@gmail.com'),
-            'phonenumber': self._settings.get('user', "phonenumber"),
-            'companyname': self._settings.get('user', "companyname"),
-            'address1': self._settings.get('address', "address"),
-            'city': self._settings.get('address', "city"),
-            'state': self._settings.get('address', "state"),
-            'postcode': self._settings.get('address', "zipcode"),
-            'country': self._settings.get('address', 'countrycode'),
-            'password': self._settings.get('user', "password"),
-            'password2': self._settings.get('user', "password")
-        }
-
-        res = requests.post(self.BASE_URL+'/purchase', json=data, verify=False)
+        data = self.json_user_config()
+        res = requests.post(self.BASE_URL+'/purchase/'+option.purchase_url, json=self.json_user_config(), verify=False)
         print(res)
         pay_url = res.content.decode('utf8')
         print(pay_url)
         self.pay(wallet, self.get_gateway(), pay_url)
-
-    '''
-    Hoster-specific methods that are needed to perform the actions
-    '''
-
-    def _server_form(self):
-        """
-        Fills in the form containing server configuration.
-        :return:
-        """
-        form = self._browser.select_form('form#frmConfigureProduct')
-        self._fill_server_form()
-        try:
-            form['configoption[61]'] = '657'  # Ubuntu 16.04
-        except LinkNotFoundError:
-            form['configoption[125]'] = '549'  # Ubuntu 16.04
-        self._browser.submit_selected()
-
-    @classmethod
-    def _parse_openvz_hosting(cls, page):
-        table = page.find('table', {'class': 'plans-block'})
-        details = table.tbody.tr
-        names = table.findAll('div', {'class': 'plans-title'})
-        i = 0
-        for plan in details.findAll('div', {'class': 'plans-content'}):
-            name = names[i].text.strip() + ' OVZ'
-            option = cls._parse_openvz_option(plan, name)
-            i = i + 1
-            yield option
-
-    @staticmethod
-    def _parse_openvz_option(plan, name):
-        elements = plan.findAll("div", {'class': 'info'})
-        eur = float(plan.find('div', {'class': 'plans-price'}).span.text.replace('\u20AC', ''))
-        option = VpsOption(
-            name=name,
-            storage=elements[0].text.split(' GB')[0],
-            cores=elements[1].text.split(' vCore')[0],
-            memory=elements[2].text.split(' GB')[0],
-            bandwidth='unmetered',
-            connection=int(elements[4].text.split(' GB')[0]) * 1000,
-            price=round(CurrencyRates().convert("EUR", "USD", eur), 2),
-            purchase_url=plan.a['href'],
-        )
-        return option
-
-    @classmethod
-    def _parse_kvm_hosting(cls, page):
-        table = page.find('table', {'class': 'plans-block'})
-        details = table.tbody.tr
-        names = table.findAll('div', {'class': 'plans-title'})
-        i = 0
-        for plan in details.findAll('div', {'class': 'plans-content'}):
-            name = names[i].text.strip() + ' KVM'
-            option = cls._parse_kvm_option(plan, name)
-            i = i + 1
-            yield option
-
-    @staticmethod
-    def _parse_kvm_option(plan, name):
-        elements = plan.findAll("div", {'class': 'info'})
-        eur = float(plan.find('div', {'class': 'plans-price'}).span.text.replace('\u20AC', ''))
-        option = VpsOption(
-            name=name,
-            storage=elements[0].text.split(' GB')[0],
-            cores=elements[1].text.split(' vCore')[0],
-            memory=elements[3].text.split(' GB')[0],
-            bandwidth='unmetered',
-            connection=int(elements[4].text.split(' GB')[0]) * 1000,
-            price=round(CurrencyRates().convert("EUR", "USD", eur), 2),
-            purchase_url=plan.a['href'],
-        )
-        return option
-
-    @staticmethod
-    def _extract_vi_from_links(links):
-        for link in links:
-            if "_v=" in link.url:
-                return link.url.split("_v=")[1]
-        return False
 
     @staticmethod
     def _check_login(text):
